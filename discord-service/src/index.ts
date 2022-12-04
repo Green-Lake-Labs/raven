f43285c174c86a4889bef3b26e641a5703dd5da6
jpcaulfi <joecaulfield29@yahoo.com>
2022-12-03 19:43:13 -0600

    discord v0

--- discord-service/src/index.ts
+++ discord-service/src/index.ts
@@ -1,6 +1,7 @@
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
 import path from 'path';
+import { exampleEmbed } from './embed';
 
 dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
 
@@ -9,7 +10,9 @@ const client = new Discord.Client({intents: ["Guilds", "GuildMessages"]});
 client.on("messageCreate", function(message) { 
     if (!message.author.bot) {
         message.reply('This is a test');
+        message.reply({ embeds: [exampleEmbed] });
     };
 }); 
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+console.log("\n>- Bot running")
