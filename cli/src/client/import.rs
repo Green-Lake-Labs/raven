bb424cf7abc5f10783021084c201b599305fbce4
jpcaulfi <joecaulfield29@yahoo.com>
2022-12-04 12:41:03 -0600

    hydration timer

--- cli/src/client/import.rs
+++ cli/src/client/import.rs
@@ -1,4 +1,17 @@
+use crate::raven_cli::DataType;
+
 const RAVEN_URL: &str = "http://localhost:3002";
+
+pub async fn import(data_type: &DataType, symbol: &str) -> Result<String, Box<dyn std::error::Error>> {
+    println!();
+    match data_type {
+        DataType::All => import_all_data(&symbol).await?,
+        DataType::Financial => import_financial_data(&symbol).await?,
+        DataType::Price => import_historical_price_data(&symbol).await?,
+        DataType::Profile => import_profile_data(&symbol).await?,
+    };
+    Ok(format!("Import of {} data for {} finished.", data_type, symbol))
+}
 
 pub async fn import_profile_data(symbol: &str) -> Result<String, Box<dyn std::error::Error>> {
     let query = [("symbol", symbol)];
